78f5e50ae0f98c796d6bc1cae487612e628ff806
a.chebykin <a.chebykin@betinvest.com>
2021-12-17 15:11:31 +0200

    code rework, remove switch, and colors

--- script.js
+++ script.js
@@ -2,17 +2,8 @@ let startButton = document.getElementById('start');
 startButton.addEventListener('click', start.bind(this));
 
 function start() {
-    alert('Данный сервис все еще в разработке и работает только с тремя странами.(Украина, Польша, Венгрия)');
     let value = prompt('Укажите сумму покупок в месной валюте');
-    let country = prompt('Уточните страну покупки');
-    switch (country) {
-        case 'Украина': alert(`Вы можете вернуть ${value * 0.2} UAH.`);
-        break;
-        case 'Польша': alert(`Вы можете вернуть ${value * 0.23} PLN.`);
-        break;
-        case 'Венгрия': alert(`Вы можете вернуть ${value * 0.27} HUF.`);
-        break;
-    }
+    alert(`Вы можете вернуть ${value * 0.23} PLN.`);
     money()
 }
 
@@ -21,15 +12,5 @@ function money() {
     let money = document.createElement("img");
     money.src = 'img/money.jpeg';
     wrap.append(money);
-}
-
-let colors = document.querySelectorAll('.colorTag')
-colors.forEach((color)=>{
-    color.addEventListener('click', textColor.bind(this, color.id));
-});
-
-function textColor(color) {
-    let text = document.getElementById('tax');
-    text.style.color = color;
 }
 
