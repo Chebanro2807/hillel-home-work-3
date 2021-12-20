2cbb662f150fb67943ce1aa3b752257dd0b783a9
a.chebykin <a.chebykin@betinvest.com>
2021-12-20 17:13:02 +0200

    Add render and poorPicture. Rework old functions. Add conditions

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 let startButton = document.getElementById('start');
 startButton.addEventListener('click', start.bind(this));
+let wrap = document.getElementById('moneyWrap');
 
 function start() {
-    let output = `Вы можете вернуть ${calculateTax()} PLN.`
-    document.getElementById('result').innerHTML = output;
-    money()
+    let taxValue = calculateTax();
+    if (taxValue >= 0){
+        render()
+        document.getElementById('result').innerHTML = text(taxValue);
+        if (taxValue > 10000) {
+            addPictureRich()
+        } else {
+            addPicturePoor()
+        }
+    } else {
+        alert('Но ведь нельзя купить в минус(')
+    }
+}
+
+function text(value) {
+    return `Вы можете вернуть ${value} PLN.`
 }
 
 function calculateTax() {
-    let value = document.getElementById('cash').value * 0.23;
-    return value
+    return document.getElementById('cash').value * 0.23;
 }
 
-function money() {
-    let wrap = document.getElementById('moneyWrap');
-    let money = document.createElement("img");
-    money.src = 'img/money.jpeg';
-    if (!wrap.firstChild) {
-        wrap.append(money);
+function addPictureRich() {
+    let img = document.createElement("img");
+    img.src = 'img/money.jpeg';
+    wrap.append(img);
+}
+
+function addPicturePoor() {
+    let img = document.createElement("img");
+    img.src = 'img/poor.jpeg';
+    wrap.append(img);
+}
+
+function render() {
+    if (wrap.firstChild) {
+        wrap.firstChild.remove();
     }
 }
+
 
